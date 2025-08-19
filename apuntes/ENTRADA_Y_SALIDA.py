576e886c27ebd786b99605f0979323420b70b246
braianfloresc <braian.flores.ig@gmail.com>
2025-08-19 13:51:00 -0300

    apuntes

---
+++ apuntes/ENTRADA_Y_SALIDA.py
@@ -0,0 +1,5 @@
+#ejemplificacion de input y output en Python
+print( "Hola bienvenido a Python")  
+nombre = input("¿Cuál es tu nombre? ")
+print("Hola", nombre, "espero que estes bien")
+
---
+++ apuntes/F_STRINGS.py
@@ -0,0 +1,9 @@
+nombre = "braian"
+edad = 22
+
+print("Hola mi nombre es", nombre, "y tengo", edad, "años")
+print("Hola mi nombre es " + nombre + " y tengo " + str(edad) + " años")
+
+#F-STRINGS dejamos todo mas ordenado
+
+print(f"Hola mi nombre es {nombre} y tengo {edad} años")
