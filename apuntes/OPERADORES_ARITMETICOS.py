f52838564767d9c19481ed6cb0347dad6836c8f4
braianfloresc <braian.flores.ig@gmail.com>
2025-08-19 14:08:02 -0300

    act2

--- apuntes/OPERADORES_ARITMETICOS.py
+++ apuntes/OPERADORES_ARITMETICOS.py
@@ -7,3 +7,21 @@ print( 9//5) # División entera
 print( 9//5) # Módulo (resto de la división)
 print( 9**5) # Exponente (potencia) 
 print( 9%5)  # Módulo (resto de la división)
+
+#x= "10"
+#y = int(x)
+#print(y)
+
+#x=5
+#y=2.5
+#z= x + y
+#print(z) # Resultado: 7.5
+
+#nombre = "Ana"
+#edad = 25
+#print(f"{nombre} tiene {edad} años")
+
+precio = 19.99
+cantidad = 3
+total = precio * cantidad
+print(f"el total es : {total}")
