7e7a12eb18779b229579ddb28fbe388825d3bf68
braianfloresc <braian.flores.ig@gmail.com>
2025-08-20 22:24:14 -0300

    word 1

---
+++ unidad_1/word_ejercicios.py
@@ -0,0 +1,118 @@
+#Programación 1
+
+#1.Indica si los siguientes identificadores son válidos en Python. En el caso de que el identificador no sea válido, explica el motivo.
+"""
+alumno1
+        VÁLIDO
+1alumno
+        NO VÁLIDO empieza con numero
+primerNombre
+        NO VÁLIDO utiliza camelCase
+/apellido
+        NO VÁLIDO empieza con caracter especial
+tamaño_máximo
+        NO VÁLIDO utiliza tilde
+for
+        NO VÁLIDO palabra reservada
+_$nombre
+        NO VÁLIDO utiliza caracter especial
+global
+        NO VÁLIDO palabra reservada
+primer_nombre
+        VÁLIDO
+num_mayor
+        VÁLIDO
+menor-num
+        NO VÁLIDO  utiliza operador
+dni@alumno
+        NO VÁLIDO utiliza caracter especial
+5var
+        NO VÁLIDO comienza con numero
+with
+        NO VÁLIDO palabra reservada
+Auto-seleccionado
+        NO VÁLIDO utiliza operador
+%aumento
+        NO VÁLIDO utiliza operador
+_123
+        VÁLIDO
+ValorTotal
+        NO VÁLIDO utiliza PascalCase
+DESCUENTO
+        VÁLIDO
+año
+        NO VÁLIDO utiliza caracter especial
+mes_actual
+        VÁLIDO
+apellido&nombre
+        NO VÁLIDO utiliza caracter especial
+89GW5
+        NO VÁLIDO comienza con numero
+valido?
+        NO VÁLIDO utiliza caracter especial
+
+"""
+#2.Indica qué dato se guarda en la variable x en cada caso, suponiendo una ejecución secuencial del programa.
+
+"""
+
+a)	x=30
+b)	x=30
+c)	x=25
+d)  x=8
+e)	x=13
+f)	x=8
+
+
+"""
+#3.Indica qué tipo de dato se guarda en cada variable.
+
+"""
+a) float
+b) float
+c) int
+d) int
+e) str
+f) str
+g) str
+h) int
+i) int
+j) float
+k) float
+l) str
+m) bool
+n) bool
+o) bool
+
+"""
+#4.Indica cuáles de las siguientes operaciones no son válidas.
+
+"""
+a) valida
+b) valida
+c) no valida
+d) no valida
+e) no valida
+f) valida
+g) valida
+h) valida
+i) no valida
+j) no valida
+k) no válida
+l) no valida
+
+"""
+#5.Declara una variable de cada tipo de dato y asígnale un valor.
+
+"""
+int = 22
+float = 22.2
+complex = 2 + 2j
+string = “chinchilla”
+bool = true
+list = [“2”, “22”, pipicucu, “chinchilla”]
+tuple = (“2”, “22”, true, "pamplinas")
+dict = {“nombre”: “braian”, “apellido”: “Flores”, “edad”: 22}
+null = None
+
+"""
